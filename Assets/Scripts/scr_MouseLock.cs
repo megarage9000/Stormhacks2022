3fc282979368498b55fa8b972ae1c6deb6ec5e78
sleepsim <91400544+sleepsim@users.noreply.github.com>
2022-02-19 01:08:16 -0800

    Pause menu functionality

--- Assets/Scripts/scr_MouseLock.cs
+++ Assets/Scripts/scr_MouseLock.cs
@@ -6,11 +6,15 @@ public class scr_MouseLock : MonoBehaviour
 {
     private bool pauseToggle;
     private DefaultInput defaultInput;
+    public GameObject pauseMenu;
 
     private void Awake()
     {
         defaultInput = new DefaultInput();
         defaultInput.Enable();
+        pauseMenu = GameObject.Find("PauseMenu");
+        pauseMenu.SetActive(false);
+        
     }
 
     void Update()
@@ -21,19 +25,29 @@ public class scr_MouseLock : MonoBehaviour
         {
             if (pauseToggle)
             {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-                Debug.Log("Menu Off / Cursor Locked");
+                ResumeGame();
             }
             else
             {
-                Debug.Log("Menu On / Cursor Unlocked");
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
+                PauseGame();
             }
 
             pauseToggle = !pauseToggle;
         }
+    }
+
+    public void PauseGame()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        pauseMenu.SetActive(true);
+    }
+    public void ResumeGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+        pauseToggle = !pauseToggle;
     }
 
 }
